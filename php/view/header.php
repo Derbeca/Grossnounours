98d3377f20b3700ccca3d614d7e1f0c9c4891a00
Derbeca <derbeca@hotmail.com>
2020-03-02 16:47:32 +0100

    corrections php

--- php/view/header.php
+++ php/view/header.php
@@ -19,18 +19,16 @@
         <nav>
             <a href="#" id="loginClick"><img src="../CRECHE/assets/svg/icon_login.svg"></a>
             <ul id="login">
-                <li><a href="http:accueil.php">Login</a></li>
-                <li><a href="http:accueil.php">Logout</a></li>
-                <li><a href="http:accueil.php">Inscriptions</a></li>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="logout.php">Logout</a></li>
+                <li><a href="inscription.php">Inscription</a></li>
             </ul>
             <a href="#" id="menuClick"><img src="../CRECHE/assets/images/boutonMenu.png"></a>
             <ul id="menu">
-                <li><a href="http:accueil.php">Accueil</a></li>
-                <li><a href="http:presentation.php">Présentation</a></li>
-                <li><a href="http:creches.php">Crèches</a></li>
-                <li><a href="http:tarifs.php">Tarifs</a></li>
-                <li><a href="http:inscription.php">Inscription</a></li>
-                <li><a href="http:contact.php">Contact</a></li>
+                <li><a href="accueil.php">Accueil</a></li>
+                <li><a href="creches.php">Crèches</a></li>
+                <li><a href="tarifs.php">Tarifs</a></li>
+                <li><a href="contact.php">Contact</a></li>
             </ul>           
         </nav>
     </header>
